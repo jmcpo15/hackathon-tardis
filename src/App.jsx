dc84f1e4e71a85de9dca87a7f344573fea118535
Lawrence Card <lcard1987@gmail.com>
2022-06-09 16:17:11 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
             <button id="news" onClick={() => setCategory("news")}>news</button>
             <button id="bitesize" onClick={() => setCategory("bitesize")}>bitesize</button>
             <button id="sounds" onClick={() => setCategory("sounds")}>sounds</button>
-            <button id="iplayer"onClick={() => setCategory("iplayer")} >iplayer</button>
+            <button id="iplayer" onClick={() => setCategory("iplayer")} >iplayer</button>
             <TimelineWrapper type={currentType} era={selectedEra} clickHandler={handleClick} modalState={modalShow} modalHandler={modalHandler} category={category} />
         </div>
         <div className="sideBody" />
@@ -115,7 +115,7 @@ function App() {
           {
             modalContent.contentUrl &&
               <div className='contentLink'>
-                <a href={`${modalContent.contentLink}`} target="_blank" rel="noreferrer">Go to content {modalContent.contentLink}</a>
+                <img src={`${modalContent.contentUrl}`} />
               </div>
           }
 
--- src/components/timelines/ReactVerticalTimelineComponent.jsx
+++ src/components/timelines/ReactVerticalTimelineComponent.jsx
@@ -10,21 +10,31 @@ const generateTimelineBlocks = (tlContent, type, clickHandler, modalState, modal
   if (type === 'era') {
     return (
       <VerticalTimelineElement
-      key={`${index + 5}-${content.title}`}
-      className="vertical-timeline-element--work"
-      contentStyle={content.contentStyle}
-      contentArrowStyle={content.contentArrowStyle}
-      date={content.date}
-      iconStyle={content.iconStyle}
-      icon={content.icon ? Logo(content.icon) : ''}
-      onTimelineElementClick={() => clickHandler(content.title)}
-    >
-      <h3 className="vertical-timeline-element-title">{content.title}</h3>
-      <h4 className="vertical-timeline-element-subtitle">{content.subTitle}</h4>
-      <p>
-        {content.bodyText}
-      </p>
-    </VerticalTimelineElement>
+        key={`${index + 5}-${content.title}`}
+        className="vertical-timeline-element--work"
+        contentStyle={ {...content.contentStyle }}
+        contentArrowStyle={content.contentArrowStyle}
+        // date={content.date}
+        iconStyle={content.iconStyle}
+        icon={content.icon ? Logo(content.icon) : ''}
+        onTimelineElementClick={() => clickHandler(content.title)}
+      >
+        <div className='eraTitle'>
+          <span className='eraTitleH2'>{content.title}</span>
+          {/* <h3 className=''>{content.date}</h3> */}
+        </div>
+        <div style={{backgroundImage: `url(${content.background})`, backgroundSize: '100% 100%', minHeight: '500px'}}> </div>
+
+        { !content.background && 
+          <div>
+            <h3 className="vertical-timeline-element-title">{content.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{content.subTitle}</h4>
+            <p>
+              {content.bodyText}
+            </p>
+          </div>
+        }
+      </VerticalTimelineElement>
     )
   }
 
@@ -53,7 +63,7 @@ export default function ReactVerticalTimelineComponent(props) {
   return (
     <>
     {era && 
-      <div className='eraTitle'>
+      <div className='eraBreakdownTitle'>
         <button onClick={() => clickHandler('', true)}>{'<'}</button>
         <h2>{era}</h2>
       </div>}
